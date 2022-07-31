b2daeb9ae4ad84435b6b6416141bbd8a86b133bd
Nia Watson <nia.d.watson@gmail.com>
2022-07-31 13:39:37 -0500

    add files and some styling. Selecting all attritubes now in budget.js.

---
+++ js/budget.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Selecting the elements
+const balanceEl = document.querySelector(".balance.value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+const incomeEL = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income .list");
+const expenseList =document.querySelector("#expense .list");
+const allList = document.querySelector("#all .list");
+
+// Selecting the toggle buttons
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector("tab2");
+const allBtn = document.querySelector("tab3");
+
+// Selecting the input buttons for adding and expense and income
+const addExpense = document.querySelector("")
