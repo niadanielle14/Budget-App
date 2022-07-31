9885b8bc73071845db51bfae3f93110743d63f2f
Nia Watson <nia.d.watson@gmail.com>
2022-07-31 15:24:24 -0500

    added event listeners but cant get the click function to work.

--- js/budget.js
+++ js/budget.js
@@ -13,8 +13,84 @@ const allList = document.querySelector("#all .list");
 
 // Selecting the toggle buttons
 const expenseBtn = document.querySelector(".tab1");
-const incomeBtn = document.querySelector("tab2");
-const allBtn = document.querySelector("tab3");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
 
 // Selecting the input buttons for adding and expense and income
-const addExpense = document.querySelector("")
+const addExpense = document.querySelector(".add-expense");
+const expenseTitle = document.getElementById("#expense-title-input");
+const expenseAmount = document.getElementById("#expense-amount-input");
+
+const addIncome = document.querySelector(".add-expense");
+const incomeTitle = document.getElementById("#income-title-input");
+const  incomeAmount = document.getElementById("#income-amount-input");
+// use getElementById when using the input attribute.
+
+
+// Variables
+let ENTRY_LIST = [];
+let balance = 0, income = 0, outcome = 0;
+
+const DELETE = "delete";
+const EDIT = "edit";
+
+// Event Listeners for buttons to be clickable. Showing and hiding/active and inactive select buttons
+expenseBtn.addEventListener("click", function(){
+    show(expenseEl);
+    hide([incomeEL, allEl]);
+    active(expenseBtn);
+    inactive([incomeBtn, allBtn]);
+})
+incomeBtn.addEventListener("click", function(){
+    show(incomeEL);
+    hide([expenseEl, allEl]);
+    active(incomeBtn);
+    inactive([expenseBtn, allBtn]);
+})
+allBtn.addEventListener("click", function(){
+    show(allEl);
+    hide([expenseEl, incomeEL]);
+    active(allBtn);
+    inactive([expenseBtn, incomeBtn]);
+})
+
+// Functions to show, hide, active, and inactive
+function show(element){
+    element.classList.remove("hide");
+}
+ function hide(elements){
+    elements.forEach(element => {
+        element.classList.add("hide");
+    })
+ }
+
+function active(element){
+    element.classList.add("active");
+}
+function inactive(elements){
+    elements.forEach(element => {
+        element.classList.remove("inactive");
+    })
+}
+
+
+
+
+
+
+
+// addIncome.addEventListener("click", function(){
+//
+// })
+// addExpense.addEventListener("click", function(){
+//
+// })
+// incomeList.addEventListener("click", function(){
+//
+// })
+// expenseList.addEventListener("click", function(){
+//
+// })
+// allList.addEventListener("click", function(){
+//
+// })
